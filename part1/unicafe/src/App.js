932318d2afd6398031a1fb0e54b1f470433e0775
JaelAguilar <71282327+JaelAguilar@users.noreply.github.com>
2021-06-16 14:28:45 -0500

    Step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const Button = ({ text, func }) => (<button onClick={func}>{text}</button>)
 
-const Stats = ({ text, count }) => (<div><p>{text} {count}</p></div>)
+const Stats = ({ text, count, perc }) => (<div><p>{text} {count} {perc}</p></div>)
 
 const Title = ({ title }) => (<div><h2>{title}</h2></div>)
 
@@ -11,9 +11,32 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+
+  //Sets all the stats
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+ 
+  const increaseGood = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+    setAverage((good - bad + 1) / (all + 1))
+    setPositive(100 * (good + 1) / (all + 1))
+  }
+  
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage((good - bad) / (all + 1))
+    setPositive(100*(good) / (all + 1))
+  }
+  const increaseBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setAverage((good - bad - 1) / (all + 1))
+    setPositive(100*(good) / (all + 1))
+  }
 
   return (
     <div>
@@ -26,8 +49,12 @@ const App = () => {
       <Stats text="Good" count={good}></Stats>
       <Stats text="Neutral" count={neutral}></Stats>
       <Stats text="Bad" count={bad}></Stats>
+      <Stats text="All" count={all}></Stats>
+      <Stats text="Average" count={average}></Stats>
+      <Stats text="Positive" count={positive} perc="%"></Stats>
     </div>
   )
 }
 
 export default App;
+
