4b7ea900a9bf8a24c99d847cfc8d22c4a122777c
Jael Aguilar <cjaelaguilar@gmail.com>
2021-06-23 22:37:47 -0500

    Put all components in a different module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,56 +1,6 @@
 import React from "react"
+import Curriculum from "./components/Curriculum"
 
-const Title = ({ text }) => (
-  <div>
-    <h1>{text}</h1>
-  </div>
-)
-
-const Part = ({ part }) => (
-  <div>
-    <p>
-      {part.name}, {part.exercises}
-    </p>
-  </div>
-)
-
-const Header = ({name}) => (
-  <div>
-    <h2>{name}</h2>
-  </div>
-)
-
-const Content = ({ parts }) => (
-parts.map((part)=><Part key={part.id} part={part} />)
-)
-
-const Total = ({ parts }) => (
-  <div>
-    <p>
-      <strong>
-        Total of{" "}
-        {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
-      </strong>
-    </p>
-  </div>
-)
-
-const Course = ({ course }) => (
-  <div>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-)
-
-const Curriculum = ({ curriculum }) => (
-  <div>
-    <Title text="Web development curriculum" />
-    {curriculum.courses.map((course) => (
-      <Course course={course} />
-    ))}
-  </div>
-)
 
 const App = () => {
   const curriculum = {
