73f32d743be3118e3fe205f18d6efc2f57222ac2
Jael Aguilar <cjaelaguilar@gmail.com>
2021-06-23 20:17:40 -0500

    Add Course component

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -34,30 +34,40 @@ const Total = (props) => (
   </div>
 )
 
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   }
 
   return (
     <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course}/>
     </div>
   )
 }
