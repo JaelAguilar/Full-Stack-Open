fc93b1ed5a71132bdee5e7e240fbf9d9ae1443a3
Jael Aguilar <cjaelaguilar@gmail.com>
2021-06-30 12:36:06 -0500

    Changed index.js to only the necessary and in App.js now you can add new Persons

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name:'Arto Hellas'}
+  ])
+
+  const [newName, setNewName] = useState('')
+
+  const handleNewName = (event) => {
+    console.log(event.target.value)
+  setNewName(event.target.value)
+  }
+  
+  const addPerson = (event) => {
+    event.preventDefault() //prevent reload
+    console.log('submit',newName)
+    const personObject = {
+      name:newName,
+      id:persons.length+1
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
+      <div>{persons.map(person => <div>{person.name}</div>)}</div>
     </div>
-  );
+  )
 }
 
 export default App;
